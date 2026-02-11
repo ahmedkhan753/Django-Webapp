24315a0b89c3e9d784d2285671cdb8080a10b152
ahmedkhan753 <ahmedk32410@gmail.com>
2026-02-11 13:16:12 +0500

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    context={
+        "variable":"hello this is a test variable",
+        "variable2":"this is another test variable"
+    }
+    return render(request, "index.html", context)
+
+def about(request):
+    return HttpResponse("this is about page")
